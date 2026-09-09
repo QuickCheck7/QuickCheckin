e4e78d210071c14d7f7f53767e75255164340523
Jatin Maurya <mauryajatin45@gmail.com>
2026-09-10 02:01:42 +0530

    fix: add pending_approval to SubscriptionHistory action enum

--- backend/models/SubscriptionHistory.js
+++ backend/models/SubscriptionHistory.js
@@ -9,7 +9,7 @@ const subscriptionHistorySchema = new mongoose.Schema({
   },
   action: {
     type: String,
-    enum: ['created', 'upgraded', 'downgraded', 'renewed', 'canceled', 'payment_failed', 'payment_succeeded', 'refunded', 'trial_started', 'trial_ending'],
+    enum: ['created', 'upgraded', 'downgraded', 'renewed', 'canceled', 'payment_failed', 'payment_succeeded', 'refunded', 'trial_started', 'trial_ending', 'pending_approval'],
     required: true
   },
   fromPlan: {
@@ -30,6 +30,10 @@ const subscriptionHistorySchema = new mongoose.Schema({
     type: String,
     enum: ['USD', 'CAD'],
     default: 'USD'
+  },
+  stripeSubscriptionId: {
+    type: String,
+    default: null
   },
   stripeInvoiceId: {
     type: String,
